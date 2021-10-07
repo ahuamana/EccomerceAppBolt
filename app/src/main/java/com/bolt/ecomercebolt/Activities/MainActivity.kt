7d18bf14209ebd13e6c83eae95eaa33a754416bf
Alikhan <73962071@continental.edu.pe>
2021-10-07 06:50:21 -0500

    added design for activity main

--- app/src/main/java/com/bolt/ecomercebolt/Activities/MainActivity.kt
+++ app/src/main/java/com/bolt/ecomercebolt/Activities/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.bolt.ecomercebolt.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.bolt.ecomercebolt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun goToSignUp(view: View) {
+
+        val intent = Intent (this, RegisterActivity::class.java)
+        startActivity(intent)
+
+    }
+    fun goToLogin(view: View) {
+        val intent = Intent (this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
